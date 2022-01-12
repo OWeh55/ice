e2213bbf07f5ce3cee94a1d7ebd8889f6e875bf3
Wolfgang Ortmann <noo@uni-jena.de>
2022-01-12 09:32:09 +0100

    base class for vector operations v1,v2 -> v1[,v2]

---
+++ src/VWorker.cpp
@@ -0,0 +1,274 @@
+/*
+ * ICE - C++ - Library for image processing
+ *
+ * Copyright (C) 1992..2018 FSU Jena, Digital Image Processing Group
+ * Copyright (C) 2019..2022 Wolfgang Ortmann
+ * Contact: ice@ortmann-jena.de
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; if not, see <http://www.gnu.org/licenses/>.
+ */
+
+using namespace std;
+#include "VWorker.h"
+
+namespace ice
+{
+#define CNAME "VWorker"
+
+  void VWorker::checkIndex(unsigned int i) const
+  {
+    if (i > 1)
+      throw IceException(CNAME, "input index out of range");
+  }
+
+  void VWorker::setParameter(int newSize)
+  {
+    size = newSize;
+    resultValid = false;
+  }
+
+  void VWorker::setInput(const double* v, int n)
+  {
+    setInput(0, v, n);
+  }
+
+  void VWorker::setInput(int i, const double* v, int n)
+  {
+    checkIndex(i);
+    checkParameter(n);
+    input[i].resize(size);
+    for (int k = 0; k < size; k++)
+      input[i][k] = v[k];
+  }
+
+  void VWorker::setInput(const double* vr, const double* vi, int n)
+  {
+    setInput(0, vr, n);
+    setInput(1, vr, n);
+  }
+
+  void VWorker::setInput(const std::vector<double>& v)
+  {
+    setInput(0, v);
+  }
+
+  void VWorker::setInput(int i, const std::vector<double>& v)
+  {
+    checkIndex(i);
+    checkParameter(v.size());
+    input[i].resize(size);
+    for (int k = 0; k < size; k++)
+      input[i][k] = v[k];
+  }
+
+  void VWorker::setInput(const std::vector<double>& vr,
+                         const std::vector<double>& vi)
+  {
+    setInput(0, vr);
+    setInput(1, vr);
+  }
+
+  void VWorker::setInput(const std::vector<Point>& v)
+  {
+    checkParameter(v.size());
+    input[0].resize(size);
+    input[1].resize(size);
+    for (int k = 0; k < size; k++)
+      {
+        input[0][k] = v[k].x;
+        input[0][k] = v[k].y;
+      }
+  }
+
+  void VWorker::setInput(const std::vector<int>& v, double factor)
+  {
+    setInput(0, v, factor);
+  }
+
+  void VWorker::setInput(int i, const std::vector<int>& v, double factor)
+  {
+    checkIndex(i);
+    checkParameter(v.size());
+    input[i].resize(size);
+    for (int k = 0; k < size; k++)
+      input[i][k] = v[k];
+  }
+
+  void VWorker::setInput(const std::vector<int>& vr,
+                         const std::vector<int>& vi, double factor)
+  {
+    setInput(0, vr, factor);
+    setInput(1, vr, factor);
+  }
+
+  void VWorker::setInputFromRow(const ice::matrix<double>& m, int row)
+  {
+    setInputFromRow(0, m, row);
+  }
+
+  void VWorker::setInputFromRow(int i, const ice::matrix<double>& m, int row)
+  {
+    checkIndex(i);
+    checkParameter(m.cols());
+    input[i].resize(size);
+    m.getRow(row, input[i]);
+  }
+
+  void VWorker::setInputFromRow(const ice::matrix<double>& m1,
+                                const ice::matrix<double>& m2, int row)
+  {
+    setInputFromRow(0, m1, row);
+    setInputFromRow(1, m2, row);
+  }
+
+
+  void VWorker::setInputFromRow(const ice::matrix<int>& m, int row, double factor)
+  {
+    setInputFromRow(0, m, row, factor);
+  }
+
+  void VWorker::setInputFromRow(int i, const ice::matrix<int>& m, int row, double factor)
+  {
+    checkIndex(i);
+    checkParameter(m.cols());
+    input[i].resize(size);
+    m.getRow(row, input[i], factor);
+  }
+
+  void VWorker::setInputFromRow(const ice::matrix<int>& m1,
+                                const ice::matrix<int>& m2, int row, double factor)
+  {
+    setInputFromRow(0, m1, row, factor);
+    setInputFromRow(1, m2, row, factor);
+  }
+
+
+  void VWorker::setInputFromColumn(const ice::matrix<double>& m, int col)
+  {
+    setInputFromColumn(0, m, col);
+  }
+
+  void VWorker::setInputFromColumn(int i, const ice::matrix<double>& m, int col)
+  {
+    checkIndex(i);
+    checkParameter(m.rows());
+    input[i].resize(size);
+    m.getColumn(col, input[i]);
+  }
+
+  void VWorker::setInputFromColumn(const ice::matrix<double>& m1,
+                                   const ice::matrix<double>& m2, int col)
+  {
+    setInputFromColumn(0, m1, col);
+    setInputFromColumn(1, m2, col);
+  }
+
+
+  void VWorker::setInputFromColumn(const ice::matrix<int>& m, int col, double factor)
+  {
+    setInputFromColumn(0, m, col, factor);
+  }
+
+  void VWorker::setInputFromColumn(int i, const ice::matrix<int>& m, int col, double factor)
+  {
+    checkIndex(i);
+    checkParameter(m.rows());
+    input[i].resize(size);
+    m.getColumn(col, input[i], factor);
+  }
+
+  void VWorker::setInputFromColumn(const ice::matrix<int>& m1,
+                                   const ice::matrix<int>& m2, int col, double factor)
+  {
+    setInputFromColumn(0, m1, col, factor);
+    setInputFromColumn(1, m2, col, factor);
+  }
+
+  void VWorker::getResult(double* v1,
+                          double* v2, int n) const
+  {
+    checkDone();
+
+    if (result[1].empty())
+      throw IceException(CNAME, "result only one dimensional");
+
+    if (size != n)
+      throw IceException(CNAME, M_VECTORDIM);
+
+    for (int k = 0; k < size; k++)
+      {
+        v1[k] = result[0][k];
+        v2[k] = result[1][k];
+      }
+  }
+
+  void VWorker::getResult(double* v, int n) const
+  {
+    checkDone();
+
+    if (size != n)
+      throw IceException(CNAME, M_VECTORDIM);
+
+    for (int k = 0; k < size; k++)
+      {
+        v[k] = result[0][k];
+      }
+  }
+
+  void VWorker::getResult(std::vector<double>& v1,
+                          std::vector<double>& v2) const
+  {
+    checkDone();
+
+    if (result[1].empty())
+      throw IceException(CNAME, "result only one dimensional");
+
+    v1.resize(size);
+    v2.resize(size);
+
+    for (int k = 0; k < size; k++)
+      {
+        v1[k] = result[0][k];
+        v2[k] = result[1][k];
+      }
+  }
+
+  void VWorker::getResult(std::vector<double>& v) const
+  {
+    checkDone();
+
+    v.resize(size);
+
+    for (int k = 0; k < size; k++)
+      {
+        v[k] = result[0][k];
+      }
+  }
+
+  void VWorker::getResult(std::vector<Point>& vp) const
+  {
+    checkDone();
+
+    if (result[1].empty())
+      throw IceException(CNAME, "result only one dimensional");
+
+    vp.resize(size);
+
+    for (int k = 0; k < size; k++)
+      {
+        vp[k] = Point(result[0][k], result[1][k]);
+      }
+  }
+
+}
---
+++ src/VWorker.h
@@ -0,0 +1,138 @@
+/*
+ * ICE - C++ - Library for image processing
+ *
+ * Copyright (C) 1992..2018 FSU Jena, Digital Image Processing Group
+ * Copyright (C) 2019..2022 Wolfgang Ortmann
+ * Contact: ice@ortmann-jena.de
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; if not, see <http://www.gnu.org/licenses/>.
+ */
+#ifndef VWORKER_H
+#define VWORKER_H
+
+#include <vector>
+#include "matrixtemplate.h"
+#include "Point.h"
+namespace ice
+{
+  // abstract base class for operations on vectors like fourier transform
+  // or convolution
+  //
+  class VWorker
+  {
+  public:
+    VWorker(): size(0), resultValid(false)
+    {}
+    VWorker(int size): size(size), resultValid(false)
+    {}
+
+    virtual ~VWorker()
+    {
+    }
+
+    VWorker(const VWorker& ft) = delete;
+    const VWorker& operator=(const VWorker& ft) = delete;
+
+    virtual void setParameter(int newSize);
+
+    // assign input data in different forms
+    void setInput(const double* v, int n);
+    void setInput(int i, const double* v, int n);
+    void setInput(const double* vr, const double* vi, int n);
+
+    void setInput(const std::vector<double>& v);
+    void setInput(int i, const std::vector<double>& v);
+    void setInput(const std::vector<double>& vr,
+                  const std::vector<double>& vi);
+
+    void setInput(const std::vector<Point>& v);
+
+    void setInput(const std::vector<int>& v, double factor = 1.0);
+    void setInput(int i, const std::vector<int>& v, double factor = 1.0);
+    void setInput(const std::vector<int>& vr,
+                  const std::vector<int>& vi, double factor = 1.0);
+
+    // input from row of 2d matrix
+    void setInputFromRow(const ice::matrix<double>& v, int row);
+    void setInputFromRow(int i, const ice::matrix<double>& v, int row);
+    void setInputFromRow(const ice::matrix<double>& vr,
+                         const ice::matrix<double>& vi, int row);
+
+    void setInputFromRow(const ice::matrix<int>& v,
+                         int row, double factor = 1.0);
+    void setInputFromRow(int i, const ice::matrix<int>& v,
+                         int row, double factor = 1.0);
+    void setInputFromRow(const ice::matrix<int>& vr,
+                         const ice::matrix<int>& vi,
+                         int row, double factor = 1.0);
+
+    // input from column of 2d matrix
+    void setInputFromColumn(const ice::matrix<double>& v, int col);
+    void setInputFromColumn(int i, const ice::matrix<double>& v, int col);
+    void setInputFromColumn(const ice::matrix<double>& vr,
+                            const ice::matrix<double>& vi, int col);
+
+    void setInputFromColumn(const ice::matrix<int>& v, int col,
+                            double factor = 1.0);
+    void setInputFromColumn(int i, const ice::matrix<int>& v, int col,
+                            double factor = 1.0);
+    void setInputFromColumn(const ice::matrix<int>& vr,
+                            const ice::matrix<int>& vi,
+                            int col, double factor = 1.0);
+
+    // get Result
+    void getResult(double* v1,
+                   double* v2, int n) const;
+
+    void getResult(double* v1, int n) const;
+
+    void getResult(std::vector<double>& v1,
+                   std::vector<double>& v2) const;
+
+    void getResult(std::vector<double>& v1) const;
+
+    void getResult(std::vector<Point>& v1) const;
+
+    void getResultFromReal(std::vector<double>& v1,
+                           std::vector<double>& v2) const;
+
+    void getResultFromImag(std::vector<double>& v1,
+                           std::vector<double>& v2) const;
+
+    // put Result into row of 2d matrix
+    void getResultToRow(ice::matrix<double>& m, int row) const;
+    void getResultToRow(ice::matrix<double>& m1,
+                        ice::matrix<double>& m2, int row) const;
+
+    // put Result into column of 2d matrix
+    void getResultToColumn(ice::matrix<double>& m, int col) const;
+    void getResultToColumn(ice::matrix<double>& m1,
+                           ice::matrix<double>& m2, int col) const;
+
+  private:
+
+    virtual void transform() const = 0;
+
+    void checkIndex(unsigned int i) const;
+    void checkParameter(int size);
+
+    void checkDone() const;
+
+    int size = 0;
+    bool resultValid = false;
+    std::array<std::vector<double>, 2> input;
+    mutable std::array<std::vector<double>, 2> result;
+  };
+}
+#endif
