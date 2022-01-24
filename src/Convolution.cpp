311b62bd4d401d5f6cd95b74d4ca77ce6ec46967
Wolfgang Ortmann <noo@uni-jena.de>
2022-01-24 14:05:57 +0100

    initial revision

---
+++ src/Convolution.cpp
@@ -0,0 +1,58 @@
+/*
+ * ICE - C++ - Library for image processing
+ *
+ * Copyright (C) 2022..2022 Wolfgang Ortmann
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; if not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "Convolution.h"
+#include "FourierTrafo.h"
+
+using std::vector;
+
+namespace ice
+{
+#define CNAME "Convolution"
+
+  void Convolution::transform() const
+  {
+    FourierTrafo ft1(size);
+    ft1.setInput(input[0], input[1]);
+    // spectrum first input
+    vector<double> real1(size);
+    vector<double> imag1(size);
+    ft1.getResultFromReal(real1, imag1);
+    // spectrum first input
+    vector<double> real2(size);
+    vector<double> imag2(size);
+    ft1.getResultFromImag(real2, imag2);
+    // spectrum result
+    vector<double> real3(size);
+    vector<double> imag3(size);
+
+    double sqrtN = sqrt(size);
+    // convolution is product in frequency domain
+    for (int i = 0; i < size; i++)
+      {
+        real3[i] = sqrtN * (real1[i] * real2[i] - imag1[i] * imag2[i]);
+        imag3[i] = sqrtN * (real1[i] * imag2[i] + real1[i] * imag2[i]);
+      }
+
+    FourierTrafo ft2(size, true);
+    ft2.setInput(real3, imag3);
+    result[0].resize(size);
+    ft2.getResult(result[0]);
+  }
+}
---
+++ src/Convolution.h
@@ -0,0 +1,52 @@
+/*
+ * ICE - C++ - Library for image processing
+ *
+ * Copyright (C) 2021 FSU Jena, Digital Image Processing Group
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; if not, see <http://www.gnu.org/licenses/>.
+ */
+#ifndef CONVOLUTION_H
+#define CONVOLUTION_H
+
+#include <vector>
+#include "VWorker.h"
+
+namespace ice
+{
+  class Convolution : public VWorker
+  {
+  public:
+    Convolution(int size): VWorker(size)
+    {
+    }
+
+    Convolution(): VWorker()
+    {}
+
+    virtual ~Convolution()
+    {
+    }
+
+    Convolution(const Convolution& ft) = delete;
+    const Convolution& operator=(const Convolution& ft) = delete;
+
+    void convolute(const std::vector<double>& src1,
+                   const std::vector<double>& src2,
+                   std::vector<double>& dst);
+
+  private:
+    virtual void transform() const;
+  };
+}
+#endif
