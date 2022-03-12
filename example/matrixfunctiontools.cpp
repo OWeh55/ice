40695b274d4e776e4aea4cf3dc48a2b40db7fec9
Wolfgang Ortmann <noo@uni-jena.de>
2022-03-12 11:26:31 +0100

    first 2d functions (delta, gauss)

--- example/matrixfunctiontools.cpp
+++ example/matrixfunctiontools.cpp
@@ -50,60 +50,48 @@ void printc(const matrix<double>& v1, const matrix<double>& v2,
   cout << "-----" << endl;
 }
 
-typedef void (*testFunction)(double para, vector<double>& v);
+typedef void (*testFunction)(double para1, double para2, matrix<double>& v);
 
 // delta
-void setFunction0(double para, vector<double>& v) // \delta
+void setFunction0(double para1, double para2, matrix<double>& v) // \delta
 {
-  int size = v.size();
-  for (int i = 0; i < size; i++)
-    v[i] = 0.0;
-  int i0 = (int(para) % size + size) % size;
-  v[i0] = 1.0;
+  int nCols = v.cols();
+  int nRows = v.rows();
+  for (int r = 0; r < nRows; r++)
+    for (int c = 0; c < nCols; c++)
+      v[r][c] = 0.0;
+  int c0 = nCols / 2;
+  c0 = ((c0 + int(para1)) % nCols + nCols) % nCols;
+  int r0 = nRows / 2;
+  r0 = ((r0 + int(para2)) % nRows + nRows) % nRows;
+  v[r0][c0] = 1.0;
 }
 
-// sin
-void setFunction1(double para, vector<double>& v)
+// gauss like
+void setFunction1(double para1, double para2, matrix<double>& v) // \delta
 {
-  int size = v.size();
-  if (para == 0.0) para = 1.0;
-  for (int i = 0; i < size; i++)
-    {
-      v[i] = sin(para * 2 * M_PI * i / size);
-    }
+  int nCols = v.cols();
+  double c0 = nCols / 2;
+  int nRows = v.rows();
+  double r0 = nRows / 2;
+  if (para1 == 0.0)
+    para1 = 1;
+  if (para2 == 0.0)
+    para2 = 1;
+  for (int r = 0; r < nRows; r++)
+    for (int c = 0; c < nCols; c++)
+      {
+        double dd = (r - r0) * (r - r0) / para1 + (c - c0) * (c - c0) / para2;
+        v[r][c] = exp(-dd);
+      }
 }
 
-// cos
-void setFunction2(double para, vector<double>& v)
+void setFunction(unsigned int type, double para1, double para2, matrix<double>& v)
 {
-  int size = v.size();
-  if (para == 0.0) para = 1.0;
-  for (int i = 0; i < size; i++)
-    {
-      v[i] = cos(para * 2 * M_PI * i / size);
-    }
-}
+  vector<testFunction> func{setFunction0, setFunction1};
 
-// rectangle
-void setFunction3(double para, vector<double>& v)
-{
-  int size = v.size();
-  if (para == 0.0)
-    para = size / 2.0;
-  for (int i = 0; i < size; i++)
-    {
-      if (i < para)
-        v[i] = 1.0;
-      else
-        v[i] = 0.0;
-    }
-}
-
-void setFunction(unsigned int type, double para, vector<double>& v)
-{
-  vector<testFunction> func{setFunction0, setFunction1, setFunction2, setFunction3};
   if (type < func.size())
-    func[type](para, v);
+    func[type](para1, para2, v);
   else
     {
       cerr << "undefined function type" << endl;
