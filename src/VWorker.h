e9e034bb79d87e8932aa806bd022d9fdea36482b
Wolfgang Ortmann <noo@uni-jena.de>
2022-03-21 09:15:59 +0100

    documentation (doxy)

--- src/VWorker.h
+++ src/VWorker.h
@@ -26,113 +26,342 @@
 #include "Point.h"
 namespace ice
 {
-  // abstract base class for operations on vectors like fourier transform
-  // or convolution
-  //
+  /** abstract base class for operations on vectors.
+   *  Operation has one or two input vectors and one our two results.
+   *  Input and output is supported for several data types like
+   *  c array, vector or ice::Vector. Operator can be used by
+   *  creating operator, set parameter, set input values (setInput) and
+   *  retrieve the result (getResult)
+   */
+
   class VWorker
   {
   public:
+    /**
+     * standard constructor.
+     * size will be determined from input
+     */
+
     VWorker(): size(0), resultValid(false)
     {}
+
+    /**
+     * constructor with given size of vectors.
+     * @param size size of vectors
+     */
 
     VWorker(int size): size(size), resultValid(false)
     {}
 
+    /**
+     * destructor
+     */
     virtual ~VWorker()
     {
     }
 
+    /**
+     * copy constructor deleted
+     */
     VWorker(const VWorker& ft) = delete;
+
+    /**
+     * assignment operator deleted
+     */
     const VWorker& operator=(const VWorker& ft) = delete;
 
+    /**
+     * change size of vectors
+     * input and results become invalid
+     */
     virtual void setParameter(int newSize);
 
-    // assign input data in different forms
-    // c array
+    /**
+     * assign c array to first input (index 0)
+     * @param v input vector as c array
+     * @param n size if c array
+     */
     void setInput(const double* v, int n);
+    /**
+     * assign c array to specific input
+     * @param i input number (0/1)
+     * @param v input vector as c array
+     * @param n size if c array
+     */
     void setInput(int i, const double* v, int n);
-    void setInput(const double* vr, const double* vi, int n);
+    /**
+     * assign two c arrays to input
+     * @param v1 first input vector as c array
+     * @param v2 second input vector as c array
+     * @param n size if c arrays
+     */
+    void setInput(const double* v1, const double* v2, int n);
 
-    // vector<double>
+    /**
+     * assign vector<double> to first input (index 0).
+     * @param v input vector
+     */
     void setInput(const std::vector<double>& v);
+
+    /**
+     * assign vector<double> to specific input.
+     * @param i input number (0/1)
+     * @param v input vector
+     */
     void setInput(int i, const std::vector<double>& v);
+
+    /**
+     * assign two vector<double> to input.
+     * @param v1 first input vector
+     * @param v2 second input vector
+     */
     void setInput(const std::vector<double>& vr,
                   const std::vector<double>& vi);
 
-    // vector<Point>
+
+    /**
+     * assign vector<ice::Point> to input.
+     * @param v input vector\n
+     * x coordinate is used as first vector\n
+     * y coordinate is used as second vector
+     */
     void setInput(const std::vector<Point>& v);
 
     // vector<int>
+    /**
+     * assign vector<int> to first input (index 0).
+     * @param v input vector
+     * @param factor factor all values are multiplied with
+     */
     void setInput(const std::vector<int>& v, double factor = 1.0);
+    /**
+     * assign vector<int> to specific input.
+     * @param i input number (0/1)
+     * @param v input vector
+     * @param factor factor all values are multiplied with
+     */
     void setInput(int i, const std::vector<int>& v, double factor = 1.0);
-    void setInput(const std::vector<int>& vr,
-                  const std::vector<int>& vi, double factor = 1.0);
+    /**
+     * assign two vector<double> to input.
+     * @param v1 first input vector
+     * @param v2 second input vector
+     * @param factor factor all values are multiplied with
+     */
+    void setInput(const std::vector<int>& v1,
+                  const std::vector<int>& v2, double factor = 1.0);
 
     // input from row of 2d matrix
-    void setInputFromRow(const ice::matrix<double>& v, int row);
-    void setInputFromRow(int i, const ice::matrix<double>& v, int row);
-    void setInputFromRow(const ice::matrix<double>& vr,
-                         const ice::matrix<double>& vi, int row);
+    /**
+     * assign row of matrix<double> to first input (index 0).
+     * @param m input matrix
+     * @param row row number
+     */
+    void setInputFromRow(const ice::matrix<double>& m, int row);
+    /**
+     * assign row of matrix<double> to specific input.
+     * @param i input number (0/1)
+     * @param m input matrix
+     * @param row row number
+     */
+    void setInputFromRow(int i, const ice::matrix<double>& m, int row);
+    /**
+     * assign row of matrix<double> to input.
+     * @param m1 input matrix
+     * @param m2 input matrix
+     * @param row row number
+     */
+    void setInputFromRow(const ice::matrix<double>& m1,
+                         const ice::matrix<double>& m2,
+                         int row);
 
-    void setInputFromRow(const ice::matrix<int>& v,
+    // input from row of 2d matrix
+    /**
+     * assign row of matrix<int> to first input (index 0).
+     * @param m input matrix
+     * @param row row number
+     * @param factor factor all values are multiplied with
+     */
+    void setInputFromRow(const ice::matrix<int>& m,
                          int row, double factor = 1.0);
-    void setInputFromRow(int i, const ice::matrix<int>& v,
+
+    /**
+     * assign row of matrix<int> to specific input.
+     * @param i input number (0/1)
+     * @param m input matrix
+     * @param row row number
+     * @param factor factor all values are multiplied with
+     */
+    void setInputFromRow(int i, const ice::matrix<int>& m,
                          int row, double factor = 1.0);
-    void setInputFromRow(const ice::matrix<int>& vr,
-                         const ice::matrix<int>& vi,
+
+    /**
+     * assign row of matrix<int> to input.
+     * @param m1 input matrix
+     * @param m2 input matrix
+     * @param row row number
+     * @param factor factor all values are multiplied with
+     */
+    void setInputFromRow(const ice::matrix<int>& m1,
+                         const ice::matrix<int>& m2,
                          int row, double factor = 1.0);
 
     // input from column of 2d matrix
-    void setInputFromColumn(const ice::matrix<double>& v, int col);
-    void setInputFromColumn(int i, const ice::matrix<double>& v, int col);
-    void setInputFromColumn(const ice::matrix<double>& vr,
-                            const ice::matrix<double>& vi, int col);
+    /**
+     * assign column of matrix<double> to first input (index 0).
+     * @param m input matrix
+     * @param col column number
+     */
+    void setInputFromColumn(const ice::matrix<double>& m, int col);
 
-    void setInputFromColumn(const ice::matrix<int>& v, int col,
+    /**
+    * assign column of matrix<double> to specific input.
+    * @param i input number (0/1)
+    * @param m input matrix
+    * @param col column number
+    */
+    void setInputFromColumn(int i, const ice::matrix<double>& m, int col);
+
+    /**
+    * assign column of matrix<double> to input.
+    * @param m1 input matrix
+    * @param m2 input matrix
+    * @param column column number
+    */
+    void setInputFromColumn(const ice::matrix<double>& m1,
+                            const ice::matrix<double>& m2, int col);
+
+    /**
+    * assign column of matrix<int> to first input (index 0).
+    * @param m input matrix
+    * @param col column number
+    * @param factor factor all values are multiplied with
+    */
+    void setInputFromColumn(const ice::matrix<int>& m, int col,
                             double factor = 1.0);
-    void setInputFromColumn(int i, const ice::matrix<int>& v, int col,
+    /**
+     * assign column of matrix<int> to specific input.
+     * @param i input number (0/1)
+     * @param m input matrix
+     * @param col column number
+     * @param factor factor all values are multiplied with
+     */
+    void setInputFromColumn(int i, const ice::matrix<int>& m, int col,
                             double factor = 1.0);
-    void setInputFromColumn(const ice::matrix<int>& vr,
-                            const ice::matrix<int>& vi,
+
+    /**
+     * assign column of matrix<int> to input.
+     * @param m1 input matrix
+     * @param m2 input matrix
+     * @param col column number
+     * @param factor factor all values are multiplied with
+     */
+    void setInputFromColumn(const ice::matrix<int>& m,
+                            const ice::matrix<int>& m,
                             int col, double factor = 1.0);
 
     // get result
+    /**
+     * puts result to c arrays.
+     * @param v1 c array #1
+     * @param v2 c array #2
+     * @param n size of c arrays
+     */
     void getResult(double* v1,
                    double* v2, int n) const;
 
-    void getResult(double* v1, int n) const;
+    /**
+     * puts first result to c array.
+     * second result vector is ignored, no matter if it is valid or not
+     * @param v c array
+     * @param n size of c arrays
+     */
+    void getResult(double* v, int n) const;
 
+    /**
+     * puts result to vector<double>.
+     * @param v1 vector #1
+     * @param v2 vector #2
+     */
     void getResult(std::vector<double>& v1,
                    std::vector<double>& v2) const;
 
-    void getResult(std::vector<double>& v1) const;
+    /**
+     * puts first result to vector<double>.
+     * second result vector is ignored, no matter if it is valid or not
+     * @param v vector
+     */
+    void getResult(std::vector<double>& v) const;
 
-    void getResult(std::vector<Point>& v1) const;
+    /**
+     * puts result to vector<Point>.
+     * first result vector becomes x coordinate,
+     * second result vector becomes y coordinate of Points
+     * @param v c array
+     */
+    void getResult(std::vector<Point>& v) const;
 
-    void getResultFromReal(std::vector<double>& v1,
-                           std::vector<double>& v2) const;
-
-    void getResultFromImag(std::vector<double>& v1,
-                           std::vector<double>& v2) const;
-
-    // put result into row of 2d matrix
+    // put result to row of 2d matrix
+    /**
+     * puts first result to row of matrix<double>.
+     * second result vector is ignored, no matter if it is valid or not
+     * @param m matrix
+     * @param row row number
+     */
     void getResultToRow(ice::matrix<double>& m, int row) const;
+
+    /**
+     * puts result to row of two matrix<double>.
+     * @param m1 matrix
+     * @param m2 matrix
+     * @param row row number
+     */
     void getResultToRow(ice::matrix<double>& m1,
                         ice::matrix<double>& m2, int row) const;
 
     // put result into column of 2d matrix
+    /**
+     * puts first result to column of matrix<double>.
+     * second result vector is ignored, no matter if it is valid or not
+     * @param m matrix
+     * @param col column number
+     */
     void getResultToColumn(ice::matrix<double>& m, int col) const;
+    /**
+     * puts result to column of two matrix<double>.
+     * @param m1 matrix
+     * @param m2 matrix
+     * @param col column number
+     */
     void getResultToColumn(ice::matrix<double>& m1,
                            ice::matrix<double>& m2, int col) const;
 
   protected:
+    /**
+     * transformation of data.
+     * must be redefined in derived classes.\n
+     * transforms vectors input[0] and input[2] to vectors result[0] and
+     * result[1]. if there is only one result, keep vector[1] empty
+     */
     virtual void transform() const = 0;
 
+    /**
+     * check index of input.
+     * @param i input number
+     */
     void checkIndex(unsigned int i) const;
+    /**
+     * check parameter.
+     * if not yet set parameter size is set to given value
+     * if already set check if equal given value
+     * @param size size of [input] vectors
+     */
     void checkParameter(int size);
 
     // check if transform is done and result valid
     // used in getResult*
+    /**
+     * checks if result is calculated and call transfom if neccessary
+     */
     void checkDone() const;
 
     int size = 0;
