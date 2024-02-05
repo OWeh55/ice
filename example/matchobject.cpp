4b4ccbe7829697ee288eeedbfb84b733f30bd4f6
Wolfgang Ortmann <noo@uni-jena.de>
2024-02-05 10:34:57 +0100

    test cases

--- example/matchobject.cpp
+++ example/matchobject.cpp
@@ -7,7 +7,8 @@ int main(int argc, char** argv)
   img.set(0);
   
   Show(ON, img);
-#if 0
+
+#if 1
   Contur c1 = SelContur(img, true);
 #else
   Contur c1;
@@ -17,23 +18,33 @@ int main(int argc, char** argv)
   c1.add(200, 800);
   c1.close();
 #endif
+#if 1
   Matrix tm(3, 3, 1);
   tm[0][0] = 0.9;
   tm[0][1] = 0.2;
   tm[0][2] = 50;
   tm[1][0] = -0.2;
   tm[1][1] = 0.9;
-  tm[2][0] = 0.0001;
-  tm[2][1] = 0.0001;
+  //  tm[2][0] = 0.0001;
+  //  tm[2][1] = 0.0001;
+  tm[2][0] = 0.00;
+  tm[2][1] = 0.00;
   Trafo tro(tm);
-
+#else
+  Trafo tro;
+  tro.scale(500,400,0.9);
+  tro.rotate(500,400,2);
+#endif
   cout << tro.getMatrix() << endl;
+
   Contur c2 = transform(tro, c1);
-  // SelContur(img,true);
+  // Contur c2 = SelContur(img,true);
   MarkContur(c2, 200, img);
-  Trafo tr = MatchObject(c1, c2, TRM_PROJECTIVE_NOR);
-  //Trafo tr=MatchObject(c1,c2,TRM_AFFINE);
+  //  Trafo tr = MatchObject(c1, c2, TRM_PROJECTIVE_NOR);
+  //Trafo tr = MatchObject(c1, c2, TRM_PROJECTIVE);
+   Trafo tr=MatchObject(c1,c2,TRM_AFFINE);
   cout << tr.getMatrix() << endl;
+  //  cout << tr.inverse().getMatrix() << endl;
   Contur c3 = transform(tr, c1);
   MarkContur(c3, 127, img);
   GetChar();
